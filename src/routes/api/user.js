921e37229e46edc1806b43108dfa3e044a32e514
wb <wbak123@sina.com>
2020-11-21 16:11:49 +0800

    feat:用户信息格式校验

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -3,16 +3,19 @@
  */
 const router = require('koa-router')();
 const { isExist, register } = require('../../controller/user');
+const userValidate = require('../../validator/user');
+const { genValidator } = require('../../middleWares/validator');
 
 router.prefix('/api/user');
 
 // 注册路由
-router.post('/register', async (ctx, next) => {
-    const { userName, password, gender } = ctx.request.body;
-    // 调用controller方法
-    let result = register({ userName, password, gender });
-    ctx.body = result
-})
+router.post('/register', genValidator(userValidate),
+    async (ctx, next) => {
+        const { userName, password, gender } = ctx.request.body;
+        // 调用controller方法
+        let result = register({ userName, password, gender });
+        ctx.body = result
+    })
 
 // 用户名是否存在
 router.post('/isExist', async (ctx, next) => {
